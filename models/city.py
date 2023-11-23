5d5b658ac795a9ef6e13a1b5d2cc30337d9545ff
Abdo <54995858+AbdoDev0@users.noreply.github.com>
2023-11-23 22:58:23 +0100

    AirBnB_clone_v2

--- models/city.py
+++ models/city.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""
-City class, a subclass of BaseModel
-"""
-from models.base_model import BaseModel
+""" City Module for HBNB project """
+import os
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel, Base
 
 
-class City(BaseModel):
-    """
-    A subclass of BaseModel class
-    Public class attributes:
-        state_id: (str) will be State.id
-        name:     (str)
-    """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    state_id = Column(
+        String(60), ForeignKey('states.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    places = relationship(
+        'Place',
+        cascade='all, delete, delete-orphan',
+        backref='cities'
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
--- models/review.py
+++ models/review.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""
-Review class, a subclass of BaseModel class
-"""
-from models.base_model import BaseModel
+""" Review module for the HBNB project """
+import os
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel, Base
 
 
-class Review(BaseModel):
-    """
-    A subclass of BaseModel class
-    Public class attributes:
-        place_id:            (str) will be Place.id
-        user_id:             (str) will be User.id
-        text:                (str)
-    """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ Review classto store review information """
+    __tablename__ = 'reviews'
+    place_id = Column(
+        String(60), ForeignKey('places.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    user_id = Column(
+        String(60), ForeignKey('users.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    text = Column(
+        String(1024), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
